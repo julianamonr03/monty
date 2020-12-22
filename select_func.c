8065d79fd7be6eea616a913f065e1649336741f8
jose120918 <jose.esteban0918@gmail.com>
2020-12-22 15:52:25 -0500

    File for select function to need

--- select_func.c
+++ select_func.c
@@ -9,29 +9,28 @@
  **/
 void select_func(stack_t **head, char *token, unsigned int counter)
 {
-    instruction_t selector[] = {
-        {"pall", pall},
-        {"push", push},
-        {"pint", pint},
-        {"swap", swap},
+	instruction_t selector[] = {
+		{"pall", pall},
+		{"push", push},
+		{"pint", pint},
+		{"swap", swap},
 		{"add", add},
 		{"nop", nop},
 		{"pop", pop},
 		{"sub", sub},
-		{"div", div},
 		{"mul", mul},
-        {NULL, NULL}
-    };
+		{NULL, NULL}
+	};
 
-    int travel = 0;
+	int travel = 0;
 
-    while (travel < 10)
-    {
-        if (strcmp(selector[travel].opcode, token) == 0)
-        {
-            selector[travel].f(head, counter);
-            return;
-        }
-        travel++;
-    }
+	while (travel < 9)
+	{
+		if (strcmp(selector[travel].opcode, token) == 0)
+		{
+			selector[travel].f(head, counter);
+			return;
+		}
+		travel++;
+	}
 }
