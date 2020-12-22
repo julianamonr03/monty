468aec92e1ae193a0768a70a49fa71034e3fb4b2
jose120918 <jose.esteban0918@gmail.com>
2020-12-22 15:52:09 -0500

    Header monty

--- monty.h
+++ monty.h
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <ctype.h>
+
+#define LIMITER " \n"
 
 
 /* Structures Doubly linked lists */
@@ -20,9 +24,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,8 +39,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* Prototypes basic operations */
@@ -44,9 +48,12 @@ typedef struct instruction_s
 void pall(stack_t **head, unsigned int counter);
 void push(stack_t **head, unsigned int counter);
 void pint(stack_t **head, unsigned int counter);
+void swap(stack_t **head, unsigned int counter);
 void pop(stack_t **head, unsigned int counter);
 void nop(stack_t **head, unsigned int counter);
-
+void add(stack_t **head, unsigned int counter);
+void sub(stack_t **head, unsigned int counter);
+void mul(stack_t **head, unsigned int counter);
 
 /* Selector structure */
 void select_func(stack_t **head, char *token, unsigned int counter);
