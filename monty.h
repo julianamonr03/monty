7f2fda83c5b465ac7a605a9431daa542a725ee49
jose120918 <jose.esteban0918@gmail.com>
2020-12-22 18:51:57 -0500

    Update header file

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void nop(stack_t **head, unsigned int counter);
 void add(stack_t **head, unsigned int counter);
 void sub(stack_t **head, unsigned int counter);
 void mul(stack_t **head, unsigned int counter);
+void _div(stack_t **head, unsigned int counter);
 
 /* Selector structure */
 void select_func(stack_t **head, char *token, unsigned int counter);
